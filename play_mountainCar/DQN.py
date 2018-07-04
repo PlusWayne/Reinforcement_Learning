5a49a7950a7b1542035da8b5363ed5379551d73c
wayne <xuwei0517@sjtu.edu.cn>
2018-07-04 21:06:58 +0800

    upload mountain car with DQN

--- play_mountainCar/DQN.py
+++ play_mountainCar/DQN.py
@@ -5,23 +5,24 @@ import random
 from collections import deque
 import time
 
-EPSILON = 0.9
+EPSILON = 1
 Learning_rate = 0.001
-REPLAY_SIZE = 10000
-BATCH_SIZE = 32
-GAMMA = 0.9
+REPLAY_SIZE = 100000
+BATCH_SIZE = 64
+GAMMA = 0.99
 
 
 # create DQN class
 class DQN():
-    # ini
-    def __init__(self, env):
+    # init
+    def __init__(self, env, train_writer=None):
         self.replay_buffer = deque()
         self.time_step = 0
         self.epsilon = EPSILON
         self.state_dim = env.observation_space.shape[0]
 
         self.action_dim = env.action_space.n
+        self.train_writer = train_writer
 
         self.create_Q_network()
         self.create_training_method()
@@ -33,13 +34,13 @@ class DQN():
     def create_Q_network(self):
         # network weights
         # build one hidden layer neural network with 64 nodes
-        W1 = tf.Variable(tf.truncated_normal([self.state_dim, 20]))
-        b1 = tf.Variable(tf.constant(value=0.01, shape=[20]))
+        W1 = tf.Variable(tf.truncated_normal([self.state_dim, 64]))
+        b1 = tf.Variable(tf.constant(value=0.01, shape=[64]))
         # build one hidden layer neural network with 128 nodes
         # W2 = tf.Variable(tf.truncated_normal([64, 128]))
         # b2 = tf.Variable(tf.constant(value=0.01, shape=[128]))
         #
-        W2 = tf.Variable(tf.truncated_normal([20, self.action_dim]))
+        W2 = tf.Variable(tf.truncated_normal([64, self.action_dim]))
         b2 = tf.Variable(tf.constant(value=0.01, shape=[self.action_dim]))
         # input layer
         self.state_input = tf.placeholder(tf.float32, [None, self.state_dim])
@@ -50,6 +51,8 @@ class DQN():
         # Q Value layer
         # i.e.output layer
         self.Q_value = tf.matmul(h_layer, W2) + b2
+        #self.writer = tf.summary.FileWriter('F:/Reinforcement_Learning/play_mountainCar/log',tf.get_default_graph())
+        #self.writer.close()
 
     def create_training_method(self):
         # define placeholder for Q-network
@@ -63,6 +66,9 @@ class DQN():
         Q_action = tf.reduce_sum(tf.multiply(self.Q_value, self.action_input), axis=1)
 
         self.cost = tf.reduce_mean(tf.square(self.y_input - Q_action))
+
+        #tf.summary.scalar('Loss',self.cost)
+        #self.merged = tf.summary.merge_all()
 
         self.optimizer = tf.train.AdamOptimizer(Learning_rate).minimize(self.cost)
 
@@ -100,6 +106,12 @@ class DQN():
             else:
                 y_batch.append(reward_batch[i] + GAMMA * np.max(Q_value_batch[i]))
 
+        #summary = self.session.run(self.merged,feed_dict={
+            # self.state_input: state_batch,
+            # self.action_input: action_batch,
+            # self.y_input: y_batch})
+        #self.train_writer.add_summary(summary,global_step=self.time_step)
+
         self.optimizer.run(feed_dict={
             self.state_input: state_batch,
             self.action_input: action_batch,
@@ -110,67 +122,87 @@ class DQN():
         Q_value = self.Q_value.eval(feed_dict={self.state_input: [state]})[0]
 
         if random.random() <= self.epsilon:
-            return random.randint(0, self.action_dim - 1)
+        	random_action = random.randint(0, self.action_dim - 1)
+        	if random_action == 1:
+        		random_action = 2
+        	return random_action
         else:
             return np.argmax(Q_value)
 
     def action(self, state):
-        return np.argmax(self.Q_value.eval(feed_dict={
+        action = np.argmax(self.Q_value.eval(feed_dict={
             self.state_input: [state]
         })[0])
+        return action if action !=1 else 2 
 
 
 EPISODE = 10000  # Episode limitation
-STEP = 200  # Step limitation in an episode
-TEST = 10  # The number of experiment test every 100 episode
-
+STEP = 4000  # Step limitation in an episode
+TEST = 5  # The number of experiment test every 100 episode
+ENV_NAME = 'MountainCar-v0' # 'CartPole-v0' 'MountainCar-v0'
 
 def main(is_training):
     if is_training:
-        CURRENT_REWARD = -50
-        env = gym.make('MountainCar-v0') # 'CartPole-v0' 'MountainCar-v0'
-        env._max_episode_steps = 200
-        agent = DQN(env)
+        CURRENT_REWARD = -150
+        env = gym.make(ENV_NAME) 
+        env._max_episode_steps = 4000
+        agent = DQN(env=env, train_writer = tf.summary.FileWriter('F:/Reinforcement_Learning/play_mountainCar/log'))
 
         for episode in range(EPISODE):
             state = env.reset()
             for step in range(STEP):
                 action = agent.egreedy_action(state)
                 next_state, reward, done, _ = env.step(action)
-
                 # reward = 1 if done else 0
                 # perceive and train
                 agent.perceive(state, action, reward, next_state, done)
                 state = next_state
                 if done:
-                    print(step)
-                    agent.epsilon *= 0.995
-                    agent.epsilon = max(0.05,agent.epsilon)
+                    print('episode:',episode ,'step:', step)
+                    agent.epsilon *= 0.95
+                    agent.epsilon = max(0.1,agent.epsilon)
                     break
                     # Test every 100 episodes
-                if episode % 100 == 0 and episode != 0:
-                    total_reward = 0
-                    for i in range(TEST):
-                        state = env.reset()
-                        for j in range(STEP):
-                            env.render()
-                            action = agent.action(state)  # direct action for test
-                            state, reward, done, _ = env.step(action)
-                            total_reward += reward
-                            if done:
-                                break
-                    ave_reward = total_reward / TEST
-                    print('episode: ', episode, 'Evaluation Average Reward:', ave_reward)
-                    # if ave_reward >= CURRENT_REWARD:
-                    #     CURRENT_REWARD = ave_reward
-                    #     print('Start to save model...')
-                    #     saver = tf.train.Saver(max_to_keep=1)
-                    #     model_path = "model/model.ckpt"
-                    #     save_path = saver.save(agent.session, model_path)
-                    #     print('Finished!')
-                    # if ave_reward >= -50:
-                    #     break
+            if episode % 100 == 0 and episode != 0:
+                total_reward = 0
+                for i in range(TEST):
+                    state = env.reset()
+                    for j in range(STEP):
+                        # env.render()
+                        action = agent.action(state)  # direct action for test
+                        state, reward, done, _ = env.step(action)
+                        total_reward += reward
+                        if done:
+                            break
+                ave_reward = total_reward / TEST
+                print('episode: ', episode, 'Evaluation Average Reward:', ave_reward)
+                if ave_reward >= CURRENT_REWARD:
+                    CURRENT_REWARD = ave_reward
+                    print('Start to save model...')
+                    saver = tf.train.Saver(max_to_keep=1)
+                    model_path = "model/model.ckpt"
+                    save_path = saver.save(agent.session, model_path)
+                    print('Finished!')
+                if ave_reward >= -150:
+                    break
+    else:
+        env = gym.make(ENV_NAME)
+        env._max_episode_steps = 1000
+        agent = DQN(env)
+        saver = tf.train.Saver()
+        saver.restore(agent.session, tf.train.latest_checkpoint("model/"))
+        state = env.reset()
+        total_reward = 0
+        while True:
+            env.render()
+            action = agent.action(state)
+            state, reward, done, _ = env.step(action)
+            total_reward += reward
+            time.sleep(1/12)
+            if done:
+                print('Game Over!, total_reward is ',total_reward)
+                break
 
 
 if __name__ == '__main__':
-    main(is_training=True)
+    main(is_training = False)
